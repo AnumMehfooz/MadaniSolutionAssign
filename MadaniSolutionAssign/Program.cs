442d7138ec25a94ba586560870d54e043a61e1d6
Anum Mahfooz <anum_mahfooz@hotmail.com>
2023-01-10 21:02:12 +0500

    Add project files.

---
+++ MadaniSolutionAssign/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadaniSolutionAssign
+{
+    internal class MainClass
+    {
+        public static string FirstReverse(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        } 
+
+        public static int FirstFactorial(int num)
+        {
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial is: "+FirstFactorial(4));
+            Console.WriteLine("Reverse of string is: "+FirstReverse("coderbyte"));
+            Console.ReadLine();
+        }
+    }
+}
